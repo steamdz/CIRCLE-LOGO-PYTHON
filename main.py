9cf232c2de335521bea0f2bde988440f43b6c6b9
AYMEN DEV <68467119+steamdz@users.noreply.github.com>
2021-12-21 11:31:06 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#AYMEN DEV 
+#3D حلقة بوكلي
+
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor("black")
+t.speed(10) 
+
+
+for i in range(360):
+    x = t.color('red')
+
+    t.circle(180)
+    t.left(10)
+
+t.mainloop()
